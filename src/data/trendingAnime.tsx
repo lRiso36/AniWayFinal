a8adec668b3a34faf60752e8f20a55b067629cc9
Lena Deriso <ard239@miami.edu>
2026-06-06 15:25:08 -0400

    fix asset imports for production

--- src/data/trendingAnime.tsx
+++ src/data/trendingAnime.tsx
@@ -1,44 +1,15 @@
+import jjk from '../assets/jjk.jpg';
+import demonSlayer from '../assets/demonSlayer.jpg';
+import onePiece from '../assets/onepeice.jpg';
+import aot from '../assets/images.jpg';
+import vinland from '../assets/vinland.jpg';
+import chainsaw from '../assets/chainsaw.jpg';
+
 export const trendingAnime = [
-  {
-    id: 1,
-    rank: 1,
-    title: "Jujutsu Kaisen",
-    score: 9.12,
-    image: "/src/assets/jjk.jpg",
-  },
-  {
-    id: 2,
-    rank: 2,
-    title: "Demon Slayer",
-    score: 8.86,
-    image: "/src/assets/demonSlayer.jpg",
-  },
-  {
-    id: 3,
-    rank: 3,
-    title: "One Piece",
-    score: 8.70,
-    image: "/src/assets/onepeice.jpg",
-  },
-  {
-    id: 4,
-    rank: 4,
-    title: "Attack on Titan",
-    score: 9.08,
-    image: "/src/assets/images.jpg",
-  },
-  {
-    id: 5,
-    rank: 5,
-    title: "Vinland Saga",
-    score: 8.79,
-    image: "/src/assets/vinland.jpg",
-  },
-  {
-    id: 6,
-    rank: 6,
-    title: "Chainsaw Man",
-    score: 8.50,
-    image: "/src/assets/chainsaw.jpg",
-  },
+  { id: 1, rank: 1, title: "Jujutsu Kaisen", score: 9.12, image: jjk },
+  { id: 2, rank: 2, title: "Demon Slayer", score: 8.86, image: demonSlayer },
+  { id: 3, rank: 3, title: "One Piece", score: 8.70, image: onePiece },
+  { id: 4, rank: 4, title: "Attack on Titan", score: 9.08, image: aot },
+  { id: 5, rank: 5, title: "Vinland Saga", score: 8.79, image: vinland },
+  { id: 6, rank: 6, title: "Chainsaw Man", score: 8.50, image: chainsaw },
 ];
